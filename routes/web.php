3485a9f8d147bba949038d0f50da8973284cc003
“Devgen194” <devgen194@gmail.com>
2024-07-23 13:04:19 +0100

    Tech test

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AreaController;
 
-Route::get('/', [AreaController::class, 'index']);
+Route::get('/', [AreaController::class, 'index'])->name('areas.index');
 Route::post('/store', [AreaController::class, 'store'])->name('areas.store');
 
